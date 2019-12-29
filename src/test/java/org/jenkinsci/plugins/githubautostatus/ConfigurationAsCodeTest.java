b9dcdf7defe08e01677442464738ce316e692dd7
Jeff Pearce <jxpearce@godaddy.com>
2019-12-29 08:57:17 -0800

    Add casc compat tests

---
+++ src/test/java/org/jenkinsci/plugins/githubautostatus/ConfigurationAsCodeTest.java
@@ -0,0 +1,57 @@
+package org.jenkinsci.plugins.githubautostatus;
+
+import hudson.ExtensionList;
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.ConfiguratorRegistry;
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.model.CNode;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static io.jenkins.plugins.casc.misc.Util.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ConfigurationAsCodeTest {
+
+    @ClassRule
+    @ConfiguredWithCode("configuration-as-code.yml")
+    public static JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+
+    @Test
+    public void should_support_configuration_as_code() {
+        BuildStatusConfig config = ExtensionList.lookupSingleton(BuildStatusConfig.class);
+        assertNotNull(config);
+        assertEquals(2, config.getDbVersion().intValue());
+        assertEquals("test-creds", config.getCredentialsId());
+        assertTrue(config.getEnableGithub());
+        assertTrue(config.getEnableHttp());
+        assertTrue(config.getEnableInfluxDb());
+        assertTrue(config.getEnableStatsd());
+        assertEquals("test-http-creds", config.getHttpCredentialsId());
+        assertEquals("http://localhost:8088", config.getHttpEndpoint());
+        assertTrue(config.getHttpVerifySSL());
+        assertTrue(config.getIgnoreSendingTestCoverageToInflux());
+        assertTrue(config.getIgnoreSendingTestResultsToInflux());
+        assertEquals("jenkins", config.getInfluxDbDatabase());
+        assertEquals("http://localhost:8086", config.getInfluxDbUrl());
+        assertEquals("bucket", config.getStatsdBucket());
+        assertEquals("http://localhost:8087", config.getStatsdHost());
+        assertEquals("1400", config.getStatsdMaxSize());
+        assertEquals("8125", config.getStatsdPort());
+    }
+
+    @Test
+    public void should_support_configuration_export() throws Exception {
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        ConfigurationContext context = new ConfigurationContext(registry);
+        CNode yourAttribute = getUnclassifiedRoot(context).get("buildStatusConfig");
+
+        String exported = toYamlString(yourAttribute);
+
+        String expected = toStringFromYamlFile(this, "configuration-as-code-expected.yml");
+
+        assertThat(exported, is(expected));
+    }
+}
