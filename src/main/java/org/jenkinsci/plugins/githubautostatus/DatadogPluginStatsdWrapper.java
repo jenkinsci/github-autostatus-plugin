55674e6f59942b2252f7a3e3deb28e741c8eb0f2
Jim Klimov <jimklimov+jenkinsci@gmail.com>
2026-04-02 14:36:43 +0200

    Bump datadog-plugin version and corresponding API
    Signed-off-by: Jim Klimov <jimklimov+jenkinsci@gmail.com>

--- src/main/java/org/jenkinsci/plugins/githubautostatus/DatadogPluginStatsdWrapper.java
+++ src/main/java/org/jenkinsci/plugins/githubautostatus/DatadogPluginStatsdWrapper.java
@@ -1,13 +1,15 @@
 package org.jenkinsci.plugins.githubautostatus;
 
-import org.datadog.jenkins.plugins.datadog.DatadogUtilities;
+import org.datadog.jenkins.plugins.datadog.clients.ClientHolder;
+import org.datadog.jenkins.plugins.datadog.metrics.Metrics;
+import org.datadog.jenkins.plugins.datadog.metrics.MetricsClient;
 
 /**
  * StatsD wrapper that uses the Datadog plugin's client.
  */
 public class DatadogPluginStatsdWrapper implements StatsdWrapper {
 
-    private String prefix;
+    private final String prefix;
 
     public DatadogPluginStatsdWrapper(String prefix) {
         this.prefix = prefix;
@@ -15,11 +17,18 @@ public class DatadogPluginStatsdWrapper implements StatsdWrapper {
 
     @Override
     public void increment(String key, int amount) {
-        DatadogUtilities.getDatadogClient().increment(prefix + "." + key, amount, null, null);
+        Metrics m = Metrics.getInstance();
+        if (m != null) {
+            while (amount > 0) {
+                m.incrementCounter(prefix + "." + key, null, null);
+                amount = amount - 1;
+            }
+        }
     }
 
     @Override
     public void time(String key, long duration) {
-        DatadogUtilities.getDatadogClient().gauge(prefix + "." + key, duration, null, null);
+        MetricsClient mc = ClientHolder.getClient().metrics();
+        if (mc != null) mc.gauge(prefix + "." + key, duration, null, null);
     }
 }
